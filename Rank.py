b38b07348875af7b8715a9de1d2bba3df7d3b81a
l@l <15998194741m@sina.cn>
2019-07-30 16:35:28 +0800

    a

--- Rank.py
+++ Rank.py
@@ -1,6 +1,6 @@
-#定义一个银行类
+# 定义一个银行类
 class Rank:
-    def init(self,username,password,rank_number,money):
+    def init(self, username, password, rank_number, money):
         pass
 
     # 存钱
@@ -12,16 +12,16 @@ class Rank:
         pass
 
     # 修改密码
-    def modifyPassword(self,password):
+    def modifyPassword(self, password):
         pass
 
-    def login(self,username,password):
+    def login(self, username, password):
         pass
+
     def __str__(self):
         pass
 
 
-
-#1,登陆 2,注册 3,取钱 4,存钱 5,修改密码 6,退出
+# 1,登陆 2,注册 3,取钱 4,存钱 5,修改密码 6,退出
 if __name__ == '__main__':
     pass
